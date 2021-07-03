8ae091151ff6efb6eedf9ccd409e42a837b4826d
a2b-it <abdev099@gmail.com>
2021-07-03 21:42:53 +0100

    initial

---
+++ apibatchs/src/main/java/com/apiweather/app/jobs/ApibatchsApplication.java
@@ -0,0 +1,16 @@
+package com.apiweather.app.jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibatchsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibatchsApplication.class, args);
+	}
+	
+	
+	
+
+}
